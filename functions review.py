aae95e9651be7ccb9f5514764af83460e2a7d8e9
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-09-16 09:11:23 -0600

    Create functions review.py

---
+++ functions review.py
@@ -0,0 +1,13 @@
+num1 = 0
+num2 = 0
+num3 = 0
+
+
+def average_of_three(num1, num2, num3):
+    sum = num1 + num2 + num3
+    quotient = sum // 3
+    
+    print("The average is", quotient)
+    
+average_of_three(10, 20, 30)
+    
